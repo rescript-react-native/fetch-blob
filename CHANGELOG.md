9ba46f4db61cabd56b8fdbe4d01cc32c47c5bc19
Christoph Knittel <christoph@knittel.cc>
2019-09-30 09:14:37 +0200

    Import rn-fetch-blob bindings from CCA repo, add stuff from __template__. (#1)

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog of `@reason-react-native/fetch-blob`
+
+## X.Y.Z - 2019-XX-XX
+
+Initial release.
---
+++ src/ReactNativeFetchBlob.bs.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+var Config = { };
+
+var ResponseInfo = { };
+
+var $$Response = { };
+
+var Fetch = { };
+
+var Stat = { };
+
+var Fs = {
+  Stat: Stat
+};
+
+exports.Config = Config;
+exports.ResponseInfo = ResponseInfo;
+exports.$$Response = $$Response;
+exports.Fetch = Fetch;
+exports.Fs = Fs;
+/* No side effect */
